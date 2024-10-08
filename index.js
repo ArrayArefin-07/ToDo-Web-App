0c5aa33f398b894c5469b169584f28be021c113e
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-10-08 20:10:55 +0600

    added create Todo functionality

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ const container = document.querySelector(".container");
 const todoForm = document.querySelector(".todo-form");
 const todoInput = document.querySelector("#inputTodo");
 const todoAddButton = document.querySelector("#addTodoButton");
+const todoLists = document.getElementById("lists");
+
+// create Todo function
+const createTodo = (todoId, todoValue) => {
+  const todoElement = document.createElement("li");
+  todoElement.id = todoId;
+  todoElement.innerHTML = ` <span> ${todoValue} </span> 
+  <span> <button class="btn" id="deleteButton"> <i class="fa fa-trash"></i> </button> </span>
+  `;
+  todoLists.appendChild(todoElement);
+};
 
 // addTodo function
 const addTodo = (event) => {
   event.preventDefault();
-  console.log(todoInput.value);
+  const todoValue = todoInput.value;
+  // console.log(todoInput.value);
+
+  // generate Unique id
+  const todoId = Date.now().toString();
+  createTodo(todoId, todoValue);
 };
 
 //Here Adding listeners
